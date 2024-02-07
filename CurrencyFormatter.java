a8394e94feb5ed437c667fe97954dd00d4489724
beck2001 <muratbek.sharibekoff@gmail.com>
2024-02-07 23:32:38 +0600

    Currency formatter added

---
+++ CurrencyFormatter.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.text.*;
+
+
+public class CurrencyFormatter {
+    
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india = NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String french = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + french);
+    }
+}
+
